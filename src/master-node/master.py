3a17dc63629cb3552e4b289b5b1cf0d1136d9df8
Manish Patel <mpatel27@bu.edu>
2021-12-08 17:10:55 -0500

    init socket code

--- src/master-node/master.py
+++ src/master-node/master.py
@@ -1,1 +1,28 @@
-# Master node 
+# Manish Patel - CS 655
+# GENI Mini Project - Master Node code
+
+import socket
+import sys
+
+# Use the first command line argument accepted to be the host name or IP address of the server we want to connec to
+HOST = str(sys.argv[1])      
+# Use the second command line argument accepted to be the port number of the server we want to connect to
+PORTNUM = int(sys.argv[2])       
+
+# Initialize our socket specifying the addressing scheme to be AF_INET (so we can use Internet addresses)
+# and specifying the socket type to be SOCK_STREAM so that our data will transmit as a stream of characters
+# Source: https://cis.temple.edu/~ingargio/old/cis307s96/readings/docs/sockets.html
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Connect our socket to the specified server socket (based on the command line arguments above)
+    s.connect((HOST,PORTNUM))
+    # Prompt the user to type out the message they want to send to the server and store it
+    message = input("Enter your message: \n")
+    # Print out the message that the client has typed and wants to send to the server
+    print('Client sent', repr(message))
+    # Encode the string message into bytes and send it over to the server across the connection
+    s.sendall(message.encode())
+    # Store the response message from the server, up to a maximum size of 1024 
+    data = s.recv(1024)
+
+# Print out the message (decoded back from bytes to string) that the server responded with (which here is the same as the sent messages)
+print('Client received', repr(data.decode()))
--- src/worker-node/worker.py
+++ src/worker-node/worker.py
@@ -1,1 +1,38 @@
-# Worker nodes
+# Manish Patel - CS 655
+# GENI Mini Project - Worker Nodes code
+
+import socket
+import sys
+
+# HOST = '127.0.0.1'
+# Set the host address to 0.0.0.0 so that it can encompass all IP addresses on the local machine
+HOST = '0.0.0.0'
+# Use the second command line argument accepted to be the port number that this server will run on 
+PORTNUM = int(sys.argv[1])
+
+# Initialize our socket specifying the addressing scheme to be AF_INET (so we can use Internet addresses)
+# and specifying the socket type to be SOCK_STREAM so that our data will transmit as a stream of characters
+# Source: https://cis.temple.edu/~ingargio/old/cis307s96/readings/docs/sockets.html
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Bind this socket to this specific hostname/IP address and port number so that the client can connect to it
+    s.bind((HOST, PORTNUM))
+    # Start an infinite loop so that until we forcefully shut down the server, clients can continue to connecto the server 
+    while True:
+        # Allow the socket to wait for any clients to connect to it
+        s.listen()
+        # Accept a client connection that arrives
+        connection, address = s.accept()
+        # Using this connection with the client now...
+        with connection:
+            # Print out that we have now connected to a specific client
+            print('Connected by', address)
+            # Start an infinite loop that takes in the data stream from the client and break when there are no more bytes being sent
+            while True:
+                data = connection.recv(1024)
+                if not data:
+                    break
+                # Send all the data we just received back to the client, to "echo" its sent message 
+                connection.sendall(data)
+
+
+
